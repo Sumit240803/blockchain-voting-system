caf5f484cb8ba9ce8dbfcb3f9b730090949c17d3
Sumit240803 <goyalsumit651@gmail.com>
2025-02-15 16:26:43 +0530

    Error

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -2,11 +2,14 @@ const hre = require("hardhat");
 async function main() {
     const Voting = await hre.ethers.getContractFactory("Voting");
     const voting = await Voting.deploy();
-
+    const[deployer] = await hre.ethers.getSigners();
+    await voting.connect(deployer).addCandidate("Alice");
+await voting.connect(deployer).addCandidate("Bob");
     await voting.waitForDeployment();
-    await voting.addCandidate("Alice");
-    await voting.addCandidate("Bob");
+    console.log("Admin" ,await voting.admin());
+
     console.log("Voting contract deployed at:", await voting.getAddress());
+    console.log("Candidates" , await voting.getCandidates());
 }
 
 main().catch((err)=>{
--- frontend/app/hooks/useVotingContract.js
+++ frontend/app/hooks/useVotingContract.js
@@ -1,8 +1,9 @@
+"use client"
 import { ethers } from 'ethers';
 import React, { useEffect, useState } from 'react'
 import VotingABI from '../../VotingABI.json'
 
-const contractAddress =process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+const contractAddress ="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 const useVotingContract = () => {
   const[account , setAccount] = useState(null);
   const [contract ,setContract] = useState(null);
@@ -10,21 +11,22 @@ const useVotingContract = () => {
 
   const connectWallet = async()=>{
     if(window.ethereum){
-        const provider = await ethers.BrowserProvider(window.ethereum);
+        const provider = new ethers.BrowserProvider(window.ethereum);
         const signer = await provider.getSigner();
         setAccount(await signer.getAddress());
-        const votingContract = new ethers.Contract(contractAddress ,VotingABI,signer);
+        const votingContract = new ethers.Contract(contractAddress ,VotingABI.abi,signer);
         setContract(votingContract);
     }else{
         alert("Please install MetaMask!");
     }
   }
-  const fetchCandidates=async()=>{
-    if(contract){
-        const data = await contract.getCandidates();
-        setCandidates(data);
+  const fetchCandidates = async () => {
+    if (contract) {
+      const data = await contract.getCandidates();
+      setCandidates(data.map(([name, votes]) => ({ name, votes: Number(votes) })));
     }
-  }
+  };
+  
   const vote = async(index)=>{
     if(contract){
         await contract.vote(index);
